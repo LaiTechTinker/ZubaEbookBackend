44f3f51a9d02b4ef433431f29290ff5f3a0c4ba6
laiwola <layiibrahim32@gmail.com>
2025-11-14 22:19:32 +0100

    generate_content_added

--- controller/aiController.js
+++ controller/aiController.js
@@ -10,7 +10,7 @@ const generateOutline=async(req,res)=>{
   if(!topic){
     return res.status(404).json({message:"please provide a title"})
   }
-  const prompt=`you rae an expert ebook  outline generator.Create a comprehensive book outline based on the following requirements:
+  const prompt=`you are an expert ebook  outline generator.Create a comprehensive book outline based on the following requirements:
   Topic:"${topic}"
   ${description? `Description;${description}`:""}
   writting style:${style}
@@ -66,7 +66,42 @@ return res.status(404).json({message:"AI response is not valid json"})
 //  this code block will generate book content
 const generateContent=async(req,res)=>{
     try{
+const {chapterTitle,chapterDescription,style}=req.body;
+if(!chapterTitle){
+   return res.status(404).json({message:"please provide a chaptertitle"})
+}
+const prompt = `
+You are an expert ebook chapter writer. Create a detailed and well-structured chapter based on the following information:
 
+Chapter Title: "${chapterTitle}"
+Chapter Description: "${chapterDescription}"
+Writing Style: "${style}"
+Target Length:Comprehensive and detailed (aim 1500-2000 words)
+
+Requirements:
+1.Write in a ${style.toLowerCase()} tone through out the chapter
+2.Structure the content with clear section and smooth transition
+3.Include relevant examples,explantion as approiprate for the style
+4.Ensure the content flows logically from introduction to conclusion
+5.make the content engaging and valuable to to readers
+ Output Format:
+   -Start with compelling opening paragraph
+   -Use clear paragraph breaks for readibility
+   -inlcude embeddings if if approiprate for the content length
+   -End with strong conclusion or transition to next chapter
+   -Write in plain text without markdown formatting 
+  
+
+Generate the chapter now.
+`;
+
+
+   const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash-lite",
+    contents:prompt
+  });
+  const text=response.text
+  return res.status(200).json({content:text})
     }catch(e){
     return res.status(404).json({message:"server error ",
         error:e.message
